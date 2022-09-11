97eee9588b2ccef0ff7d4c4d386093214ce421df
Yafarrodrigo <yafarrodrigo@yahoo.com.ar>
2022-09-10 22:42:32 -0300

    agregue un pato ;)

--- api/index.js
+++ api/index.js
@@ -3,3 +3,17 @@ const server = require('./src/app.js');
 server.listen(3001, () => {
     console.log('server up (localhost:3001)');
 });
+
+/*     ..---..
+     .'  _    `.
+ __..'  (o)    :    LES DEJO UN PATO
+`..__          ;
+     `.       /
+       ;      `..---...___
+     .'                   `~-. .-')
+    .                         ' _.'
+   :                           :
+   \                           '
+    +                         J
+     `._                   _.'
+        `~--....___...---~' */
