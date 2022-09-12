a54675f185a4883edd004c34cf15b180e6801b00
Guillermo <guillermo.luciani@gmail.com>
2022-09-12 11:14:48 -0300

    Numero par e impar log

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
+    if (numero % 2 === 0) {
+        console.log("numero par");
     }
+    else {
+        console.log("numero impar")
+    }
+}
