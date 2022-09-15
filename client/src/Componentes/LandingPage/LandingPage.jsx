baa9936641e002f7e354350b3eb8c574a9b63e5c
David Gonzalez <iis_dbga@live.com>
2022-09-15 15:59:33 -0500

    landing page, falta componente login

--- client/src/Componentes/LandingPage/LandingPage.jsx
+++ client/src/Componentes/LandingPage/LandingPage.jsx
@@ -1,11 +1,25 @@
 import NavBar from "../NavBar/NavBar"
 import Landing from '../../recursos/landing.jpg'
+import UserForm from "../Usuarios/UserForm";
 
 const LandingPage = () => {
   return (
   <div>
+ 
     <NavBar/>
-    <img  src={Landing} alt="img" />
+ 
+    <div class="d-flex">
+      
+      <div>
+        <img  src={Landing} height="80%" width="100%" alt="img" />        
+      </div>
+  
+      <div>
+        <UserForm/>
+      </div>
+
+    </div>
+
     
   </div>  
   
