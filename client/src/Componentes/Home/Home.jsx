c95ba34b4662d5987d6752bb4310342b1e5aedb2
Yossef Hernandez <yossef.her25@gmail.com>
2022-09-15 16:59:12 -0500

    Se hace css de la home
    Se crea carpeta para archivos css extra de igual forma

--- client/src/Componentes/Home/Home.jsx
+++ client/src/Componentes/Home/Home.jsx
@@ -1,19 +1,18 @@
-
-
+import React from "react";
 import Filters from "./Filters";
 import NavBar from "../NavBar/NavBar";
+import "../../CssAdicional/Home.css"
 
 
 const Home = () => {
-  return (<div>
+  return (<div className="container">
     <NavBar/>
-
-
+    <div className="row row-cols-2 row-cols-lg-3">
+    <div className="col-4 col-lg-2">
     <Filters/>
-
-    <h1>Home</h1>
-
-    <h3>Ipsum Lorem</h3>
+    </div>
+    <div className="QuestionContainer">Aqui irian los componentes de preguntas lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum possimus recusandae sequi mollitia nam corporis voluptas voluptatum porro, sint dicta molestiae voluptates delectus. Ipsam, sed aliquid. Quas eveniet voluptatem deserunt!</div>
+    </div>
   </div>  );
 }
  
