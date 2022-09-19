e8816fc3ba852c37897c6ad15dfe917648679ffa
Guillermo <guillermo.luciani@gmail.com>
2022-09-19 14:59:04 -0300

    agrega consulta de preguntas por categorias

--- api/src/routes/search.js
+++ api/src/routes/search.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
 const router = Router()
-const { searchQuestion, searchAnswer, searchQA } = require('../controllers/searchController')
+const { searchQuestion, searchAnswer, searchQA, searchCategory } = require('../controllers/searchController')
 
 // /search/...
 
 router.get('/', searchQA)
 router.get('/answer', searchAnswer)
 router.get('/question', searchQuestion)
+router.get('/category', searchCategory)
 
 module.exports = router
