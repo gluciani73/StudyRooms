17fe84ecac7e89274507d957fec8e96f871f4930
David Bernardo González Alanís <96962543+iisblitz@users.noreply.github.com>
2022-09-19 09:13:21 -0500

    ruta 404 y agregar componente askquestion funcional

--- client/src/componentes/Error404/Error404.jsx
+++ client/src/componentes/Error404/Error404.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import E404 from '../../recursos/E404.gif'
+import NavBar from "../NavBar/NavBar";
 
 const Error404 = ()=>{
     return (
-        <div>Error 404 not found</div>
+        <div >
+           <div className="container">
+            <NavBar/>
+            <img src={E404} alt="not found" />
+            Error 404 not found
+            </div>
+        </div>
     )
 }
 export default Error404
--- client/src/componentes/Preguntas/AskQuestion.jsx
+++ client/src/componentes/Preguntas/AskQuestion.jsx
@@ -1,9 +1,6 @@
 import React,{useState,useEffect} from "react";
 import {addQuestions, getQuestions} from "../../Controllers/Actions/questionsActions"
-import {useDispatch, useSelector} from "react-redux";
-
-
-
+import {useDispatch} from "react-redux";
 import Navbar from '../NavBar/NavBar'
 
 function validate(input){
@@ -26,6 +23,7 @@ const AskQuestion = () => {
 
     const dispatch= useDispatch()
     const [input,setInput]=useState({
+        userId:1,
         title:"",
         description:"",
         categories:[],
@@ -39,11 +37,40 @@ const AskQuestion = () => {
         dispatch(addQuestions(input))
         alert("Create Question")
         setInput({
+            userId:1,
             title:"",
             description:"",
             categories:[]
         })
-     }
+        }
+
+     function handleChange(e){
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+        console.log(input)
+        setErrors(validate({
+            ...input,
+            [e.target.name]: e.target.value
+        }
+        ));
+        
+    }
+
+    function handleCheck(e){
+        if (e.target.checked){
+            setInput({
+                ...input,
+                [e.target.name]:e.target.value
+            })
+            setErrors(validate({
+                ...input,
+                [e.target.name]: e.target.value
+            }
+            ));
+        }
+    }
 
      useEffect(()=>{
         dispatch(getQuestions())
@@ -60,7 +87,9 @@ const AskQuestion = () => {
                         <label  className="form-label fs-2">Title</label>
                         <input type="text" 
                         className="form-control" 
-                        value={input.name} 
+                        required value={input.name}
+                        name="title" 
+                        onChange={(e)=>handleChange(e)}
                         />
                         {errors.title && (<p className="error">{errors.name}
                         </p>)}
@@ -69,8 +98,10 @@ const AskQuestion = () => {
                     <div className="mb-3">
                         <label  className="form-label fs-3">Description</label>
                         <input type="text" 
-                        className="form-control" 
-                        value={input.name} 
+                        className="form-control"
+                        name="description" 
+                        value={input.description} 
+                        onChange={(e)=>handleChange(e)}
                        />
                         {errors.description && (<p className="error">{errors.description}</p>)}
                         <div  className="form-text">Check yout grammar and be as specific as you can!</div>
@@ -79,46 +110,47 @@ const AskQuestion = () => {
                         <div className="row">                  
                       
                             <div className="col">
-                                <input type="checkbox" className="form-check-input" />
+                                <input type="checkbox" className="form-check-input" name="categories" value="Math" onChange={(e)=>handleCheck(e)}  />
                                 <label className="radio">Math</label>
+                                
                             </div>
                             <div className="col">
-                                <input type="checkbox" className="form-check-input" />
+                                <input type="checkbox" className="form-check-input" name="categories" value="History" onChange={(e)=>handleCheck(e)}  />
                                 <label className="form-check-label" >History</label>
                             </div>
                             <div className="col">
-                                <input type="checkbox" className="form-check-input" />
+                                <input type="checkbox" className="form-check-input" name="categories" value="Geography" onChange={(e)=>handleCheck(e)}  />
                                 <label className="form-check-label" >Geography</label>
                             </div>
                             <div className="col">
-                                <input type="checkbox" className="form-check-input"/>
+                                <input type="checkbox" className="form-check-input" name="categories" value="Chemisty"  onChange={(e)=>handleCheck(e)}  />
                                 <label className="form-check-label" >Chemistry</label>
                             </div>
                             <div className="col">
-                               <input type="checkbox" className="form-check-input"/>
+                               <input type="checkbox" className="form-check-input" name="categories" value="Biology" onChange={(e)=>handleCheck(e)}  />
                                 <label className="form-check-label" >Biology</label>
                             </div>
                             </div>
                             <div className="row">
                                 <div className="col">
-                                    <input type="checkbox" className="form-check-input"/>
+                                    <input type="checkbox" className="form-check-input" name="categories" value="Economics" onChange={(e)=>handleCheck(e)}  />
                                     <label className="form-check-label" >Economics</label>
                                 </div>
                                 <div className="col">
-                                    <input type="checkbox" className="form-check-input"/>
+                                    <input type="checkbox" className="form-check-input" name="categories" value="Programming" onChange={(e)=>handleCheck(e)}  />
                                     <label className="form-check-label" >Programming</label>
                                 </div>
                                 <div className="col">
-                                    <input type="checkbox" className="form-check-input"/>
-                                    <label className="form-check-label" >Filosofia</label>
+                                    <input type="checkbox" className="form-check-input" name="categories" value="Philosophy" onChange={(e)=>handleCheck(e)}  />
+                                    <label className="form-check-label" >Philosophy</label>
                                 </div>
                                 <div className="col">
-                                    <input type="checkbox" className="form-check-input"/>
+                                    <input type="checkbox" className="form-check-input" name="categories" value="Lenguages" onChange={(e)=>handleCheck(e)}  />
                                     <label className="form-check-label" >Lenguages</label>
                                 </div>
                                 <div className="col">
-                                    <input type="checkbox" className="form-check-input"/>
-                                    <label className="form-check-label" >Social Sciences</label>
+                                    <input type="checkbox" className="form-check-input" name="categories" value="English" onChange={(e)=>handleCheck(e)}  />
+                                    <label className="form-check-label" >English</label>
                                 </div>
                         </div>
                     </div>
