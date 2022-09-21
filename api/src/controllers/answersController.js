ee1f291e8e5f48ea7e4f889f068b8cc55d8c2a72
valdiviavv <valdiviavv@hotmail.com>
2022-09-21 08:02:07 -0400

    The merge conflicts were fixed.
    # Conflicts:
    #	api/src/controllers/answersController.js

--- api/src/controllers/answersController.js
+++ api/src/controllers/answersController.js
@@ -3,37 +3,35 @@ const { Op } = require("sequelize");
 
 const createAnswer = async (req, res) => {
     try {
-        // console.log(req.body) // .log('-------POST /answer -------------- ')
-        const { userId, questionId, answer } = req.body; //these are the mandatory fields.
+        const { userId,
+            questionId,
+            answer } = req.body;
 
-        // console.log('Posteo Answer');
         if (!answer || !userId || !questionId) {
             return res.status(401).json({
                 error: "There is a missing field, please verify userId, questionId, and answer field are present.",
                 data: null
             })
         }
-        let newAnswer = {    // creo nuevo objetos con datos de la answer pasada x body
+        let newAnswer = {
             userId,
             questionId,
-            answer,
+            answer
         }
 
         const qAnswer = await Answer.create(newAnswer);
-        // let msg = `Se creo la respuesta ${qAnswer.id}.`
+
         return res.status(201).json({ error: null, data: qAnswer })
 
     } catch (error) {
-        // console.log(error)
         return res.status(500).json({ error: 'Error en el controlador de answer', data: null })
     }
 }
 
 const getAnswer = async (req, res) => {
-    const questionId = req.params.id;
     try {
+        const questionId = req.params.id;
         if (questionId) {
-            // console.log('Respuesta params con questionId: ', questionId);
             let result = await Answer.findAll(
                 {
                     where: {
@@ -45,12 +43,11 @@ const getAnswer = async (req, res) => {
                         },
                         {
                             model: User,
-                            attributes:['id', 'avatar', 'userName', 'email']
+                            attributes: ['id', 'avatar', 'userName', 'email']
                         }
                     ]
                 }
             );
-            console.log('result trae: ', result)
             if (!result[0]) {
                 return res.status(500).send({ error: "No se encuentran respuestas para esta pregunta", data: null })
             }
