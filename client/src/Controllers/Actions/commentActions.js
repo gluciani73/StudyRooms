a38e60979fd573616c91b7e192aa8c0a86c3af4b
valdiviavv <valdiviavv@hotmail.com>
2022-09-22 05:58:09 -0400

    The ComponentList was updated to connect with the backend. Also, the action creators for comments were updated.

--- client/src/Controllers/Actions/commentActions.js
+++ client/src/Controllers/Actions/commentActions.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import {URL_BACK} from "../../constants";
 
 export const GET_COMMENT_LIST = "GET_COMMENT_LIST";
 export const CREATE_COMMENT_ITEM = "CREATE_COMMENT_ITEM";
 export const UPDATE_COMMENT_ITEM = "UPDATE_COMMENT_ITEM";
 export const DELETE_COMMENT_ITEM = "DELETE_COMMENT_ITEM";
 
-const baseUrl =  'https://w9489.mocklab.io'; //'http://localhost:3001';
+const baseUrl = URL_BACK; //'https://w9489.mocklab.io';
 
 export const getCommentList = (questionId) => {
     return function (dispatch){
@@ -14,29 +15,47 @@ export const getCommentList = (questionId) => {
             .then(response => {
                 dispatch({
                     type: GET_COMMENT_LIST,
-                    payload: response.data
+                    payload: response.data.data
                 });
             });
     }
 }
 
 export const createCommentItem = (commentItem) => {
-    return {
-        type: CREATE_COMMENT_ITEM,
-        payload: commentItem
-    };
+    return function (dispatch) {
+        axios.post(`${baseUrl}/comments`, commentItem)
+            .catch(error => console.log("Action creator createCommentItem: ", error))
+            .then(response => {
+                dispatch({
+                    type: CREATE_COMMENT_ITEM,
+                    payload: response.data.data
+                });
+            });
+    }
 }
 
 export const updateCommentItem = (commentItem) => {
-    return {
-        type: UPDATE_COMMENT_ITEM,
-        payload: commentItem
+    return function (dispatch) {
+        axios.put(`${baseUrl}/comments/${commentItem.id}`, commentItem)
+            .catch(error => console.log("Action creator updateCommentItem: ", error))
+            .then(response => {
+                dispatch({
+                    type: UPDATE_COMMENT_ITEM,
+                    payload: response.data.data
+                });
+            });
     }
 }
 
 export const deleteCommentItem = (commentItem) => {
-    return {
-        type: DELETE_COMMENT_ITEM,
-        payload: commentItem
+    return function (dispatch) {
+        axios.delete(`${baseUrl}/comments/${commentItem.id}`)
+            .catch(error => console.log("Action creator deleteCommentItem: ", error))
+            .then(() => {
+                dispatch({
+                    type: DELETE_COMMENT_ITEM,
+                    payload: {id: commentItem.id, questionId: commentItem.questionId}
+                });
+            });
     }
 }
