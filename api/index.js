86911e2030eda0b6c2ecc45fc57f893a69c40824
Guillermo <guillermo.luciani@gmail.com>
2022-09-23 18:30:18 -0300

    console.log en index

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ const { conn } = require('./src/db.js');
 const { createTestData } = require('./seeder.js')
 // const dotenv = require('dotenv')
 // dotenv.config();
-
+console.log('entro a index.js')
 conn.sync({ force: true })
   .then(() => {
+    console.log('entro en sync.then')
     server.listen(process.env.PORT, async () => {
+      console.log('entro en server.listen')
       await createTestData()
+      console.log('termino el createTestData')
       console.log('--------------------------------');
       console.log('creada el mockup de datos');
       console.log(`'server up (port: ${process.env.PORT})'`); // eslint-disable-line no-console
