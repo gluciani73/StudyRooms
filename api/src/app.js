85ca531e613d15bc17bae7831a8f1be749cf651b
Guillermo <guillermo.luciani@gmail.com>
2022-09-23 19:34:12 -0300

    incluyo morgan combined

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,7 @@ const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
-server.use(morgan('dev'));
+server.use(morgan('combined'));
 server.use(cors())
 
 
