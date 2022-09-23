59f2e3c94500d8ab3d6111d8dee2989eda3a11f9
Yossef Hernandez <yossef.her25@gmail.com>
2022-09-22 22:01:44 -0500

    Filtros terminados
    Codigo de filtro terminado, sin embargo aun no funcionan las propiedades categories ni rating de las questions

--- client/src/componentes/Home/Filters.jsx
+++ client/src/componentes/Home/Filters.jsx
@@ -1,58 +1,55 @@
 import React  from "react";
-import { useDispatch, useSelector } from "react-redux";
-
+import { useDispatch } from "react-redux";
+import { FilterCategory, FilterCategory2, SortRating } from "../../Controllers/Actions/filterHomeActions";
 const Filters = () =>{
 
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
   
-    // useEffect(()=>{  
-    //   dispatch(getQuestions())},[dispatch]);
-
-    //   function handleFilterCategory(e){
-    //     dispatch(filterQuestionCategory(e.target.value)) //falta asignar action
-    // }
-    // function handleSortRating(e){
-    //     e.preventDefault();
-    //     dispatch(orderByRating(e.target.value))
-    //     setCurrentPage(1)
-    //     setOrdenbyRating(`Ordenado ${e.target.value}`)
-    // }
+      function handleFilterCategory(e){
+        dispatch(FilterCategory(e.target.value)) //falta asignar action
+    }
+    function handleFilterCategory2(e){
+        dispatch(FilterCategory2(e.target.value)) //falta asignar action
+    }
+    function handleSortRating(e){
+        e.preventDefault();
+        dispatch(SortRating(e.target.value))
+    }
 
     return(
         <div>
-            <h1>Filtros</h1>
-            <p>Categoria</p>
+            <h1>Filters</h1>
+            <p>Category</p>
             <select className="form-select" onChange={e=>handleFilterCategory(e)}>
-                <option value="todas">Todas</option>
-                <option value="Ingles">Ingles</option>
-                <option value="Matematicas">Matematicas</option>
-                <option value="Historia">Historia</option>
-                <option value="Geografia">Geografia</option>
-                <option value="Quimica">Quimica</option>
-                <option value="Biologia">Biologia</option>
-                <option value="Economia">Economia</option>
-                <option value="Filosofia">Filosofia</option>
-                <option value="Lenguas">Lenguas</option>
+                <option value="All">All</option>
+                <option value="English">English</option>
+                <option value="Mathematics">Mathematics</option>
+                <option value="History">History</option>
+                <option value="Geography">Geography</option>
+                <option value="Chemistry">Chemistry</option>
+                <option value="Biology">Biology</option>
+                <option value="Economy">Economy</option>
+                <option value="Philosophy">Philosophy</option>
+                <option value="Languages">Languages</option>
             </select>
-            <p>Categoria secundaria</p>
-            <select className="form-select" onChange={e=>handleFilterCategory(e)}>
-                <option value="todas">Todas</option>
-                <option value="Ingles">Ingles</option>
-                <option value="Matematicas">Matematicas</option>
-                <option value="Historia">Historia</option>
-                <option value="Geografia">Geografia</option>
-                <option value="Quimica">Quimica</option>
-                <option value="Biologia">Biologia</option>
-                <option value="Economia">Economia</option>
-                <option value="Filosofia">Filosofia</option>
-                <option value="Lenguas">Lenguas</option>
+            <p>Second Category</p>
+            <select className="form-select" onChange={e=>handleFilterCategory2(e)}>
+                <option value="All">All</option>
+                <option value="English">English</option>
+                <option value="Mathematics">Mathematics</option>
+                <option value="History">History</option>
+                <option value="Geography">Geography</option>
+                <option value="Chemistry">Chemistry</option>
+                <option value="Biology">Biology</option>
+                <option value="Economy">Economy</option>
+                <option value="Philosophy">Philosophy</option>
+                <option value="Languages">Languages</option>
             </select>
             <p>Rating</p>
             <select className="form-select" onChange={e=>handleSortRating(e)}>
-                <option value="SinOrden">Sin orden</option>
-                <option value="Mayoramenor">Mayor a menor</option>
-                <option value="Menoramayor">Menor a mayor</option>
+                <option value="asc">Major to Minor</option>
+                <option value="des">Major to Minor</option>
             </select>
         </div>
     )
