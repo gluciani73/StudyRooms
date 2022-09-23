ee68349238784097b50bf2204d3b3a5c8a956103
Guillermo <guillermo.luciani@gmail.com>
2022-09-23 14:33:49 -0300

    comento testData que refiere a localhost

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { createTestData } = require('./seeder.js')
+// const { createTestData } = require('./seeder.js')
 
 conn.sync({ force: true })
   .then(() => {
     server.listen(process.env.PORT, async () => {
-      await createTestData()
+      // await createTestData()
       console.log('--------------------------------');
       console.log('creada el mockup de datos');
-      console.log(`'server up (port:${process.env.PORT})'`); // eslint-disable-line no-console
+      console.log(`'server up (port: ${process.env.PORT})'`); // eslint-disable-line no-console
     });
   })
 
