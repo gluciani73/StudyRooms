5df335e40b545a7082a448cb662dccc175eab9bb
Guillermo <guillermo.luciani@gmail.com>
2022-09-23 11:59:00 -0300

    agregados y modificaiones para deply

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 
-export const URL_BACK ='http://localhost:3001/';
+export const URL_BACK = '';
 export const CREATE_USER = 'CREATE_USER';
 export const GET_USERS = 'GET_USERS';
-export const GET_QUESTIONLIST= "GET_QUESTIONSLIST";
-export const ADD_QUESTION= "ADD_QUESTION";
-export const GET_DETAILS= "GET_DETAILS"
+export const GET_QUESTIONLIST = "GET_QUESTIONSLIST";
+export const ADD_QUESTION = "ADD_QUESTION";
+export const GET_DETAILS = "GET_DETAILS"
