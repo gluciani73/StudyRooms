73a069154343434edc83e7376c3ece313738dffc
valdiviavv <valdiviavv@hotmail.com>
2022-09-26 06:37:12 -0400

    The backend rating and votes information was added to the Answer code.

--- api/src/models/Answer.js
+++ api/src/models/Answer.js
@@ -18,7 +18,7 @@ module.exports = (sequelize) => {
             allowNull: false,
         },
         answer: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
         ratingAverage: {
