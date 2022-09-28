a15b573f1fdd22f10de6d3b074be17d2aaa5bb05
Yafarrodrigo <yafarrodrigo@yahoo.com.ar>
2022-09-28 18:59:14 -0300

    cambio choclo gigante por variable private

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -5,19 +5,18 @@ const router = Router()
 const {signIn, signUp, getAllUsers, getUserById, changePassword, activateAccount, recoveryPOST, recoveryGET, updateUser} = require('../controllers/usersController')
 const { userCreateValidator, userLoginValidator } = require('../middlewares/userValidators.js')
 const googleAuthRoutes = require('./googleAuth.js')
-
+const private = passport.authenticate('jwt', {session:false})
 // /users/...
 
 router.post('/signup', userCreateValidator, signUp)
 router.post('/signin', userLoginValidator ,signIn)
-router.put('/changePassword/:userId', passport.authenticate('jwt', {session:false}), changePassword)
-
+router.put('/changePassword/:userId', private, changePassword)
 router.post('/recovery', recoveryPOST)
 router.get('/recovery/:token', recoveryGET)
-router.get('/', passport.authenticate('jwt', {session:false}), getAllUsers)
-router.get('/:userId', passport.authenticate('jwt', {session:false}), getUserById)
+router.get('/',private , getAllUsers)
+router.get('/:userId', private, getUserById)
 router.get("/activateAccount/:token", activateAccount)
-router.put('/update/:userId', passport.authenticate('jwt', {session:false}), updateUser)
+router.put('/update/:userId', private, updateUser)
 
 router.use('/google', googleAuthRoutes)
 
