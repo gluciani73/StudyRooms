a965fd23a6c6372f9c264881af110e1f2d4d4584
Rodrigo Rimbau <rodrigorimbau94@gmail.com>
2022-09-29 05:33:27 -0300

    Home Css Demo

--- client/src/componentes/Home/Home.jsx
+++ client/src/componentes/Home/Home.jsx
@@ -1,12 +1,12 @@
-import React, {useEffect} from "react";
-import { useDispatch, useSelector} from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Filters from "./Filters";
 import NavBar from "../NavBar/NavBar";
 // import "../../CssAdicional/Home.css"
 import Question from "../Preguntas/Question";
-import { getQuestions, getCategories } from "../../Controllers/Actions/questionsActions";
-
+import { getQuestions } from "../../Controllers/Actions/questionsActions";
+import '../../CssAdicional/QuestionsCss.css'
 
 
 
@@ -16,34 +16,35 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(getQuestions())
-    dispatch(getCategories());
   }, [dispatch]);
+
   const allQuestions = useSelector((state) => state.questionReducer.allQuestions.data)
+
   return (
     <div>
-      <div className="container">
-        <NavBar />
-        <div className="row">
-          <div className="col-4 col-lg-2">
-            <Filters />
-          </div>
-          <div className="col">
-            <div className="container">
-              {allQuestions?.map((e, id) => {
-                { console.log(e); }
-                return (
-                  <div key={id}>
-                    <Link to={`/QuestionDetail/${e.id}`}>
-                      <Question key={e.id} title={e.title} description={e.description} ratingAverage={e.ratingAverage} likes={e.votesxquestions?.length}> </Question>
-                    </Link>
-                  </div>
-                )
-              })}
-            </div>
-          </div>
+      <div className="row m-0 p-0">
+        <div className="col p-0 m-0">
+          <NavBar />
         </div>
       </div>
-    </div>);
+      <div className="row m-0 p-0">
+        <div className="col-2 p-0 m-0">
+          <Filters />
+        </div>
+        <div className="col p-0 m-0 ">
+          {allQuestions?.map((e, id) => {
+            return (
+              <div key={id} className = " colQuestions">
+                <Link to={`/QuestionDetail/${e.id}`}>
+                  <Question key={e.id} title={e.title} description={e.description} ratingAverage={e.ratingAverage} likes={e.votesxquestions.length} userId={e.user.userName} > </Question>
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
- 
+
 export default Home;
--- client/src/componentes/Preguntas/Question.jsx
+++ client/src/componentes/Preguntas/Question.jsx
@@ -6,43 +6,15 @@ import stars from '../../recursos/star.png'
 
 
 export default function Question({title, description, ratingAverage, userId, likes}) {
-
- 
-    
     return(
-
-    <div>
-        <div className="row">
-        <p>title:</p>   
-        <p className="row">{title}</p>
-    
-        <div className="col">
-            <p>rating:</p>
-            <p>{ratingAverage}</p>
-            <img src={stars} alt="" height="20px" width="20px"/>
+        <div className='container containerQuestions'>
+            <h3> {title} </h3>
+            <h4> {description} </h4>
+            <p>By: { userId }</p>
+            <div className='d-flex justify-content-end'>
+                <p>Rating: { ratingAverage }</p>
+                <p>Likes: { likes } </p>
+            </div>
         </div>
-        <div className="col">
-             <p>votes:</p>
-             <p>{likes}</p>
-             <img src={upVote} alt="" height="20px" width="20px" />
-        </div>
-        
-
-
-
-        <div className="row">
-            <p>description:</p>
-            <p>{description}</p>
-        </div>
-
-        <div className="row">
-            <p>User:</p>
-            <p>{userId}</p>
-        </div>
-
-    </div>
-    </div>
-
-
-    );
+    )
 }
