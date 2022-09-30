857c4032b37b08085e97d1c53ef260e5f63bd991
valdiviavv <valdiviavv@hotmail.com>
2022-09-30 17:45:33 -0400

    [front] The Users and Categories buttons were added to the navBar.

--- client/src/componentes/NavBar/NavBar.jsx
+++ client/src/componentes/NavBar/NavBar.jsx
@@ -8,6 +8,9 @@ import { useSelector } from 'react-redux'
 
 
 const NavBar = () => {
+
+    const userInfo = useSelector(state => state.loginReducer.userInfo);
+
     return (
         <nav className='customNavbar d-flex bg-dark '>
             <div className='col d-flex justify-content-center'>
@@ -25,6 +28,12 @@ const NavBar = () => {
                     <Link to="/Home"><button type="button" className="btn btn-light m-1" >Home</button></Link>
                     <Link to="/AskQuestion"><button type="button"  className="btn btn-light m-1 " >Ask Question</button></Link>
                     <Link to="/Donations"><button type="button"  className="btn btn-light m-1" > Donate</button></Link>
+                    {userInfo && userInfo.isAdmin && (
+                        <>
+                            <Link to="/admin-panel"><button type="button" className="btn btn-danger m-1">Users</button></Link>
+                            <Link to="/categories"><button type="button" className="btn btn-danger m-1">Categories</button></Link>
+                        </>
+                    )}
                 </div>
             </div>
             <div className='col'>
