a6ced8ac6bccdcea6ae6e0dcb64f4eaf7de38a42
Rodrigo Rimbau <rodrigorimbau94@gmail.com>
2022-10-01 03:09:16 -0300

    Fix some CSS

--- client/src/componentes/Footer/Footer.jsx
+++ client/src/componentes/Footer/Footer.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../recursos/logo.png"
+import logo from "../../recursos/logo2.png"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 
 const Footer = ()=>{
     return(
         <div>
-            <footer className="text-dark py-4  bg-white">
+            <footer className="text-white py-4  bg-dark">
                 <div className="container">
                     <nav className="nav d-flex justify-content-between">
                         <div className="col-12 col-md-3 d-flex align-items-center justify-content-center" to="/">
                             <img src={logo} alt="logo" className="mx-2" width="250" />
                         </div>
-                        <ul className="col-12  col-md-3 list-unstyled  align-items-center">
+                        <ul className="col-12  col-md-3 list-unstyled align-items-center">
                             <li className="font-weight-bold mb-2 d-flex justify-content-center" >Sutudy Rooms</li>
                             <li className="text-center ">En este sitio web podremos crear, responder y puntuar preguntas sobre  temas de interes general</li>
                         </ul>
@@ -21,9 +21,9 @@ const Footer = ()=>{
                         <ul className="col-12 col-md-3 list-unstyled"> 
                             <li className=" font-weight-bold mb-2  d-flex justify-content-center" >Siguenos</li>
                             <li className="d-flex justify-content-between">
-                            <i className="bi bi-facebook"></i>
-                            <i className="bi bi-instagram"></i>
-                            <i className="bi bi-twitter"></i>
+                            <i className="bi bi-facebook icons "></i>
+                            <i className="bi bi-instagram icons"></i>
+                            <i className="bi bi-twitter icons"></i>
                             </li>
                         </ul>
                     </nav>
