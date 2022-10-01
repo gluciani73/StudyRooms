232e3316d6a749dcdb7c2b554112c1c011a677d4
Guillermo <guillermo.luciani@gmail.com>
2022-10-01 20:04:38 -0300

    middleware jwt amountDonated isPremium

--- api/src/controllers/paymentController.js
+++ api/src/controllers/paymentController.js
@@ -1,29 +1,51 @@
 // pasarela de pago
 //router.use(express.json())
+const { User } = require('../db.js')
 
 //esto es de la pasarela de pago
 
 const checkout = async (req, res) => {
     const Stripe = require("stripe")
     const stripe = new Stripe("sk_test_51LhhasEmp5dtE89LTANXUW3bFI74zGdEr5LhcWZGe6HcgGy91kP984qGYfFyaZJ9OHaIc82wGotxWrdMHPN1tf6j00i1FVnNqT")
-    
-    const {id, amount} = req.body
+
+    const { id: paymentId, amount } = req.body
 
     try {
         const payment = await stripe.paymentIntents.create({
             amount,
             currency: "USD",
-            description:"Donacion",
-            payment_method: id,
+            description: "Donacion",
+            payment_method: paymentId,
             confirm: true
-            
         })
         console.log(payment)
-        return res.send({message: "Pago realizado"})
+        ///////////////////////////////////////////////////////////////////////////
+        // registro en DB de amount y cambio a premium
+        const userReqInfo = req.user;
+        console.log('userReqInfo: ', userReqInfo)
+        const userExist = await User.findByPk(userReqInfo.id);
+        if (!userExist) return res.status(404).json({ data: null, error: "no se encontro usuario con ese id" })
+
+        let newAmount = amount;
+        amount ?
+            newAmount = userExist.amountDonated + amount
+            : newAmount = userExist.amountDonated
+
+        await User.update({
+            amountDonated: newAmount,
+            isPremium: true
+        },
+            {
+                where: { id: userReqInfo.id }
+            })
+
+        //---------------------------------------------
+        /////////////////////////////////////////////////////////
+        return res.send({ message: "Pago realizado" })
     } catch (error) {
         console.log(error)
-        return res.json({message: error.raw.message})
+        return res.json({ message: error.raw.message })
     }
 }
 
-module.exports = {checkout}
+module.exports = { checkout }
--- api/src/controllers/usersController.js
+++ api/src/controllers/usersController.js
@@ -3,21 +3,21 @@ const jwt = require('jsonwebtoken')
 const { User } = require('../db.js')
 const sendMail = require('./mailer.js')
 
-const {AUTH_SECRET, ACTIVATION_SECRET, RECOVERY_SECRET, FRONT_URL} = require('../CONSTANTS.js')
+const { AUTH_SECRET, ACTIVATION_SECRET, RECOVERY_SECRET, FRONT_URL } = require('../CONSTANTS.js')
 const mockURL = process.env.DB_LOCALHOST3001 || "https://studyrooms-deploy.herokuapp.com"
 
 const signUp = async (req, res) => {
 
     try {
-        const { userName, password, firstName, lastName, email} = req.body
-        
+        const { userName, password, firstName, lastName, email } = req.body
+
         const avatar = req.body.avatar || 'https://res.cloudinary.com/dcmn0kkly/image/upload/v1663359592/guest-user_clv1cg.jpg'
-        
+
         /* ESTO ES PARA PODER CREAR LOS USUARIOS DE TEST*/
         const active = req.body.active || false
         const isAdmin = req.body.isAdmin || false
         /*------------------------------------------------*/
-        
+
         const userFound = await User.findOne({ where: { userName: userName } })
         if (userFound) return res.status(401).json({ data: null, error: "ya existe la cuenta" })
 
@@ -44,9 +44,9 @@ const signUp = async (req, res) => {
 
         // EMAIL FOR ACTIVATION
 
-        if(active === false && userName !== "testUser3"){
-            const tokenForLink = jwt.sign({email,userName}, ACTIVATION_SECRET,{expiresIn:"1d"})
-            const activationLink = mockURL+'/users/activateAccount/' + tokenForLink
+        if (active === false && userName !== "testUser3") {
+            const tokenForLink = jwt.sign({ email, userName }, ACTIVATION_SECRET, { expiresIn: "1d" })
+            const activationLink = mockURL + '/users/activateAccount/' + tokenForLink
 
             const mailOptions = {
                 from: "study.rooms.mail@gmail.com",
@@ -58,8 +58,8 @@ const signUp = async (req, res) => {
             await sendMail(mailOptions)
         }
 
-        return res.status(200).json({ data: dataToSend, error: null})
-        
+        return res.status(200).json({ data: dataToSend, error: null })
+
     } catch (error) {
         res.status(500).json({ msg: "error in sign up controller" })
     }
@@ -73,7 +73,7 @@ const signIn = async (req, res) => {
         if (userFoundByName || userFoundByEmail) {
 
             const userFound = userFoundByName || userFoundByEmail
-            if(userFound.active === false) return res.status(403).json({data:null, error: "user needs to activate account, check email"})
+            if (userFound.active === false) return res.status(403).json({ data: null, error: "user needs to activate account, check email" })
             if (bcrypt.compareSync(password, userFound.hashedPassword)) {
                 const dataToSend = {
                     id: userFound.id,
@@ -99,63 +99,63 @@ const signIn = async (req, res) => {
     }
 }
 
-const activateAccount = async (req,res) => {
+const activateAccount = async (req, res) => {
     const token = req.params.token
     const data = jwt.verify(token, ACTIVATION_SECRET)
-    const user = await User.findOne({where:{email:data.email}})
-    if(user){
-        await User.update({active: true},{where:{email:data.email}})
+    const user = await User.findOne({ where: { email: data.email } })
+    if (user) {
+        await User.update({ active: true }, { where: { email: data.email } })
         res.redirect(FRONT_URL)
     }
-    else{
+    else {
         return res.sendStatus(404)
     }
 }
 
-const getAllUsers = async (req,res) => {
+const getAllUsers = async (req, res) => {
 
     try {
-        const results = await User.findAll({ attributes: {exclude: ['hashedPassword']}})
-        if(results){
+        const results = await User.findAll({ attributes: { exclude: ['hashedPassword'] } })
+        if (results) {
             return res.status(200).json(results)
         }
-        else{
-            return res.status(404).json({data: [], error:"no se encontraron usuarios"})
+        else {
+            return res.status(404).json({ data: [], error: "no se encontraron usuarios" })
         }
     } catch (error) {
-        return res.status(404).json({error:"falla el usersController.js", data: null})
+        return res.status(404).json({ error: "falla el usersController.js", data: null })
     }
-    
+
 }
 
-const getUserById = async (req,res) => {
+const getUserById = async (req, res) => {
 
     try {
         const id = parseInt(req.params.userId)
-        if(!id) return res.status(400).json({data:null, error: "wrong params / no user with that id"})
+        if (!id) return res.status(400).json({ data: null, error: "wrong params / no user with that id" })
 
         const results = await User.findByPk(id)
-        if(results){
-            return res.status(200).json({data: results, error: null})
+        if (results) {
+            return res.status(200).json({ data: results, error: null })
         }
-        else{
-            return res.status(404).json({data: [], error:"no se encontró el usuario con ese Id"})
+        else {
+            return res.status(404).json({ data: [], error: "no se encontró el usuario con ese Id" })
         }
     } catch (error) {
-        return res.status(404).json({error:"falla el usersController.js", data: null})
-    } 
+        return res.status(404).json({ error: "falla el usersController.js", data: null })
+    }
 }
 
-const changePassword = async (req,res) => {
-    
+const changePassword = async (req, res) => {
+
     try {
-        const {userId} = req.params
+        const { userId } = req.params
         const { password, newPassword } = req.body
         if (!userId || !password || !newPassword) {
             return res.status(404).json({ data: null, error: "faltan datos" })
         }
 
-        if(password === newPassword){
+        if (password === newPassword) {
             return res.status(404).json({ data: null, error: "no puede ser la misma contraseña" })
         }
 
@@ -163,8 +163,8 @@ const changePassword = async (req,res) => {
         if (userFound) {
             if (bcrypt.compareSync(password, userFound.hashedPassword)) {
                 const newHashedPassword = await bcrypt.hash(newPassword, bcrypt.genSaltSync(10))
-                User.update({hashedPassword: newHashedPassword}, {where:{id: parseInt(userId)}})
-            
+                User.update({ hashedPassword: newHashedPassword }, { where: { id: parseInt(userId) } })
+
                 const check = await User.findByPk(parseInt(userId))
 
 
@@ -181,16 +181,16 @@ const changePassword = async (req,res) => {
     }
 }
 
-const recoveryPOST = async (req,res) => {
+const recoveryPOST = async (req, res) => {
     try {
-        const {email} = req.body
-        if(!email) return res.status(400).json({data:null,error:"falta enviar el email"})
+        const { email } = req.body
+        if (!email) return res.status(400).json({ data: null, error: "falta enviar el email" })
 
-        const userExists = await User.findOne({where:{email}})
-        if(!userExists) return res.status(404).json({data:null, error:"user with that email does not exists in DB"})    
+        const userExists = await User.findOne({ where: { email } })
+        if (!userExists) return res.status(404).json({ data: null, error: "user with that email does not exists in DB" })
 
-        const tokenForLink = jwt.sign({email, id:userExists.id}, RECOVERY_SECRET,{expiresIn:"1d"})
-        const recoveryLink = mockURL+'/users/recovery/' + tokenForLink
+        const tokenForLink = jwt.sign({ email, id: userExists.id }, RECOVERY_SECRET, { expiresIn: "1d" })
+        const recoveryLink = mockURL + '/users/recovery/' + tokenForLink
 
         const mailOptions = {
             from: "study.rooms.mail@gmail.com",
@@ -200,77 +200,75 @@ const recoveryPOST = async (req,res) => {
         }
 
         await sendMail(mailOptions)
-        
 
-        return res.status(200).json({data: `an email has been sent to ${email} to reset your password`, error: null})
+
+        return res.status(200).json({ data: `an email has been sent to ${email} to reset your password`, error: null })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({data:null, error:"error en el userController"})
+        return res.status(500).json({ data: null, error: "error en el userController" })
     }
 }
 
-const recoveryGET = async (req,res) => {
+const recoveryGET = async (req, res) => {
 
     try {
-        const {token} = req.params
+        const { token } = req.params
 
         const data = jwt.verify(token, RECOVERY_SECRET)
-        const {email} = data
+        const { email } = data
 
         let newPassword = ""
-            for(let i = 0; i < 10; i++){
-                newPassword += Math.floor(Math.random()*10)
-            }
+        for (let i = 0; i < 10; i++) {
+            newPassword += Math.floor(Math.random() * 10)
+        }
 
-            const hashedNewPassword = await bcrypt.hash(newPassword, bcrypt.genSaltSync(10))
-            await User.update({hashedPassword: hashedNewPassword}, {where:{email}})
+        const hashedNewPassword = await bcrypt.hash(newPassword, bcrypt.genSaltSync(10))
+        await User.update({ hashedPassword: hashedNewPassword }, { where: { email } })
 
-            const mailOptions = {
-                from: "study.rooms.mail@gmail.com",
-                to: email,
-                subject: "Account password reset",
-                text: `tu password temporal es: ${newPassword}, puedes cambiarlo desde el profile`
-            }
-            await sendMail(mailOptions)
-            
-        return res.status(200).json({error:null, data: "account password reseted"})
+        const mailOptions = {
+            from: "study.rooms.mail@gmail.com",
+            to: email,
+            subject: "Account password reset",
+            text: `tu password temporal es: ${newPassword}, puedes cambiarlo desde el profile`
+        }
+        await sendMail(mailOptions)
+
+        return res.status(200).json({ error: null, data: "account password reseted" })
     } catch (error) {
-        return res.status(500).json({data:null, error:"error in userController"})
+        return res.status(500).json({ data: null, error: "error in userController" })
     }
 }
 
 const updateUser = async (req, res) => {
-    try{
-
-        // con esto saco la info del user que hizo la request con la variable "userFromRequest"
-        const {dataValues: userFromRequest} = await req.user
+    try {
+        const userFromRequest = req.user
 
         const { firstName, lastName, avatar } = req.body
         const { userId } = req.params
 
-        if ( !firstName && !lastName && !avatar) {
-            return res.status(400).json({data:null, error: "faltan datos"})
+        if (!firstName && !lastName && !avatar) {
+            return res.status(400).json({ data: null, error: "faltan datos" })
         }
 
         const userExists = await User.findByPk(userId)
-        if(!userExists) return res.status(404).json({data:null, error: "no se encontró usuario con ese id"})
+        if (!userExists) return res.status(404).json({ data: null, error: "no se encontró usuario con ese id" })
 
-        if(userExists.id !== userFromRequest.id && userFromRequest.isAdmin !== true) return res.status(403).json({error: "no podes modificar otros usuarios", data: null})
-        
+        if (userExists.id !== userFromRequest.id && userFromRequest.isAdmin !== true) return res.status(403).json({ error: "no podes modificar otros usuarios", data: null })
+
         let newAvatar = avatar
-        if(!avatar || !avatar.length){
+        if (!avatar || !avatar.length) {
             newAvatar = userExists.avatar
         }
         let newFirstName = firstName
-        if(!newFirstName || !newFirstName.length){
+        if (!newFirstName || !newFirstName.length) {
             newFirstName = userExists.firstName
         }
         let newLastName = lastName
-        if(!newLastName || !newLastName.length){
+        if (!newLastName || !newLastName.length) {
             newLastName = userExists.lastName
         }
 
-        await User.update({ ...req.body, firstName: newFirstName, lastName: newLastName, avatar: newAvatar }, {where: {id:userId}})
+        await User.update({ ...req.body, firstName: newFirstName, lastName: newLastName, avatar: newAvatar }, { where: { id: userId } })
 
         const newData = await User.findByPk(userId)
         const dataToSend = {
@@ -285,13 +283,13 @@ const updateUser = async (req, res) => {
             isPremium: newData.isPremium
         }
         const token = jwt.sign(dataToSend, AUTH_SECRET, { expiresIn: 86400 })
-        
-        return res.status(200).json({data:dataToSend, error: null, token})
-      
-    } catch (error){
+
+        return res.status(200).json({ data: dataToSend, error: null, token })
+
+    } catch (error) {
         console.log(error);
-        return res.status(500).json({data:null, error: "error en el userController"})
+        return res.status(500).json({ data: null, error: "error en el userController" })
     }
 }
 
-module.exports = { signUp, signIn, getAllUsers, getUserById,changePassword, activateAccount ,updateUser, recoveryPOST, recoveryGET}
+module.exports = { signUp, signIn, getAllUsers, getUserById, changePassword, activateAccount, updateUser, recoveryPOST, recoveryGET }
