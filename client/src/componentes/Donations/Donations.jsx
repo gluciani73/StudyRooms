b81ebe56b5a26f0ca7595467e3a98105e3861e26
Yafarrodrigo <yafarrodrigo@yahoo.com.ar>
2022-10-02 01:41:55 -0300

    fix de varios axios.post o axios.delete en el front y algun q otro detalle como un async await que no estaba

--- client/src/componentes/Donations/Donations.jsx
+++ client/src/componentes/Donations/Donations.jsx
@@ -29,12 +29,14 @@ const CheckoutForm = () => {
         })
         if (!error) {
             const { id } = paymentMethod
+            const token = localStorage.getItem("token")
             console.log(cantidad)
             try {
                 const { data } = await axios.post("/payments/checkout", {
                     id,
                     amount: cantidad * 100
-                })
+                },
+                {headers:{"Authorization":`Bearer ${token}`}})
                 console.log(data)
                 const errorData = Object.entries(data)
                 const errorAlert = errorData[0].toString().slice(8)
