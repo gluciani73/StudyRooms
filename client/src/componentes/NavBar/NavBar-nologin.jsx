d1349e403f672851be66d8d3ad88bc932dba147c
franciscofrask <franciscofrascona@gmail.com>
2022-10-04 17:17:46 -0300

    estilos de landing

--- client/src/componentes/NavBar/NavBar-nologin.jsx
+++ client/src/componentes/NavBar/NavBar-nologin.jsx
@@ -1,13 +1,13 @@
 
 import 'bootstrap'
-import logo from '../../recursos/logo.png'
+import logo from '../../recursos/logo2.png'
 
 
 const NavBarNoLogIn = () => {
   return ( 
     <div >
-    <nav className="navbar  navbar-expand-lg navbar-light bg-white">
-    <img src={logo} height="100px" alt="logo" />
+    <nav className="navbar   navbar-expand-xxl  navbar-light bg-dark">
+    <img src={logo} height="75px" width="350" alt="logo" />
     </nav>
  </div>    
 
