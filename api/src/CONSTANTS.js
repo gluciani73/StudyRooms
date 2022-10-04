a42e8a7406bfb49105a097c35f10b537b7961058
Laura <96241880+laura16ortega@users.noreply.github.com>
2022-10-04 08:41:53 -0500

    caambios

--- api/src/CONSTANTS.js
+++ api/src/CONSTANTS.js
@@ -6,4 +6,5 @@ const ACTIVATION_SECRET = process.env.ACTIVATION_SECRET || "ActivationSecret!"
 const RECOVERY_SECRET = process.env.RECOVERY_SECRET || "RecoverySecret!"
 const FRONT_URL = process.env.FRONT_URL || "https://study-rooms-gilt.vercel.app"
 
+
 module.exports = {CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, AUTH_SECRET, ACTIVATION_SECRET, RECOVERY_SECRET, FRONT_URL}
