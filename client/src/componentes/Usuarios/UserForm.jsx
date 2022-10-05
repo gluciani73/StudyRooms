fb245ac233d39533c1a67a0d08fb4eb50cd0447b
Rodrigo Rimbau <rodrigorimbau94@gmail.com>
2022-10-05 18:12:25 -0300

    UserForm Recuperado Wii

--- client/src/componentes/Usuarios/UserForm.jsx
+++ client/src/componentes/Usuarios/UserForm.jsx
@@ -1,70 +1,70 @@
 
 import React, { useState } from 'react'
-import {useDispatch} from "react-redux"
-import {createUserAction} from '../../Controllers/Actions/userAction'
+import { useDispatch } from "react-redux"
+import { createUserAction } from '../../Controllers/Actions/userAction'
 import "../../CssAdicional/Home.css"
-import { registerOnOff} from '../../Controllers/Actions/loginActions'
+import { registerOnOff } from '../../Controllers/Actions/loginActions'
 import { useSelector } from "react-redux";
 import sweetalert from 'sweetalert'
 import axios from 'axios'
 
-export default function CreateUser(){
-    const dispatch =useDispatch();
+export default function CreateUser() {
+    const dispatch = useDispatch();
     const errorLog = useSelector((state) => state.userReducer.error)
-   
+
 
 
 
     const [newUser, setNewUser] = useState({
-    userName:"",
-    firstName:"",
-    lastName:"",
-    email:"",
-    password:"",
-    ConfirmPassword:"",
-    avatar:""
-   });
+        userName: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        ConfirmPassword: "",
+        avatar: ""
+    });
 
 
-   const [checked, setChecked] = useState(false);
+    const [checked, setChecked] = useState(false);
 
-    const uploadImage = async (e) =>{
+    const uploadImage = async (e) => {
         try {
-        e.preventDefault()
-        const files = e.target.files;
-        const data = new FormData();
-        data.append("file", files[0]);
-        data.append("upload_preset", "franimages");
-    
-        const res = await axios.post(
-            "https://api.cloudinary.com/v1_1/dqffvu8gj/image/upload",data
-                
-        )
-        setNewUser({...newUser, avatar:res.data.secure_url})
-       
+            e.preventDefault()
+            const files = e.target.files;
+            const data = new FormData();
+            data.append("file", files[0]);
+            data.append("upload_preset", "franimages");
+
+            const res = await axios.post(
+                "https://api.cloudinary.com/v1_1/dqffvu8gj/image/upload", data
+
+            )
+            setNewUser({ ...newUser, avatar: res.data.secure_url })
+
         } catch (error) {
             console.log(error)
         }
-        
+
     }
-    
-    function handleChangeCheckbox(e){
+
+    function handleChangeCheckbox(e) {
         setChecked(!checked)
     }
 
-   
 
-    function handleChange(e){
+
+    function handleChange(e) {
         setNewUser({
             ...newUser,
             [e.target.name]: e.target.value
         });
-       
+
     }
-    
-    function showAlert(){
+
+    function showAlert() {
         sweetalert({
-            title:"Terminos y Condiciones",
+            title: "Terminos y Condiciones",
             text: `Es requisito necesario para la adquisición de los servicios que se ofrecen en este sitio, que lea
             y acepte los siguientes Términos y Condiciones que a continuación se redactan. El uso de
             nuestros servicios implicará que usted ha leído y aceptado los Términos y Condiciones de Uso
@@ -73,197 +73,200 @@ export default function CreateUser(){
             caso estarían sujetas a sus propios Términos y Condiciones. En algunos casos, para adquirir
             un servicio, será necesario el registro por parte del usuario, con ingreso de datos personales
             fidedignos y definición de una contraseña.`
-    })
+        })
     }
 
-    function handleRegister(e){
+    function handleRegister(e) {
         e.preventDefault()
         dispatch(registerOnOff())
-      }
-    function handleSubmit(e){
+    }
+    function handleSubmit(e) {
 
 
-        if(!checked) {
+        if (!checked) {
             alert("Por favor indica que aceptas los Términos y Condiciones");
             return false
         }
-        if(newUser.userName.length < 3 ) {
+        if (newUser.userName.length < 3) {
             alert("El nombre de usuario debe contener al menos 3 caracteres y como maximo 10")
-        }else if (newUser.password.length < 8 || newUser.password.length > 16 ){
+        } else if (newUser.password.length < 8 || newUser.password.length > 16) {
             alert("La contraseña debe contener al menos 8 caracteres y como maximo debe tener 16 caracteres")
-        
-        }else if(newUser.avatar.length < 1 ){
-            alert("Avatar not found")
-        }else{
+
+        } else {
             dispatch(createUserAction(newUser))
             setNewUser({
-                userName:"",
-                firstName:"",
-                lastName:"",
-                email:"",
-                password:"",
-                ConfirmPassword:"",
-                avatar:""
+                userName: "",
+                firstName: "",
+                lastName: "",
+                email: "",
+                password: "",
+                ConfirmPassword: "",
+                avatar: ""
             })
-            if (errorLog === "ya existe una cuenta con ese username"){
+            if (errorLog === "ya existe una cuenta con ese username") {
                 alert("Error")
-                
-            }else{
+
+            } else {
                 alert("Usuario creado correctamente")
             }
-           
-        }
-        
-
-           
-           
 
         }
- console.log(errorLog)
-        return (
 
-            <div className='d-flex flex-column align-items-center h-auto bg-light'>
-                
-                <form onSubmit={(e)=> handleSubmit(e)} className="d-flex flex-column  align-items-center text-center">
-                <h1>Register</h1>
-                {errorLog && <p className="alert alert-danger">{errorLog}</p>}
+
+
+
+
+    }
+    console.log(errorLog)
+    return (
+        <form onSubmit={(e) => handleSubmit(e)} className="d-flex flex-wrap justify-content-center bg-white p-3 container rounded">
+            {errorLog && <p className="alert alert-danger">{errorLog}</p>}
+            <div className='col-6 d-flex justify-content-center'>
+                <div className=' d-block text-center justify-content-center'>
+
                     <div>
-                   
+
                         <label htmlFor="NickName">Nickname</label>
                         <input pattern="^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$"
-                            title='El nombre de usuario debe contener entre 3 y 16 caracteres. No puede contener simbolos' 
-                            className='d-block  m-1 border-1 form-control'  
-                            type="text" value={newUser.userName} 
-                            id='userName' 
-                            name='userName' 
-                            placeholder='Nickname' 
-                            autoComplete='off'  
-                            onChange={(e)=>handleChange(e)} 
-                            required/>                  
-                        
-                   </div>
+                            title='El nombre de usuario debe contener entre 3 y 16 caracteres. No puede contener simbolos'
+                            className='d-block  m-1 border-1 form-control'
+                            type="text" value={newUser.userName}
+                            id='userName'
+                            name='userName'
+                            placeholder='Nickname'
+                            autoComplete='off'
+                            onChange={(e) => handleChange(e)}
+                            required />
+
+                    </div>
 
                     <div>
-                     
+
                         <label htmlFor="firstName">Name</label>
-                        <input pattern='[a-zA-Z]\S{1,15}' 
-                        title='Solo puede contener letras sin espacios'  
-                        className='d-block  m-1 border-1 form-control' 
-                        type="text" value={newUser.firstName} 
-                        id='firstName' name='firstName'
-                         placeholder='User Name' 
-                         autoComplete='off'  
-                         onChange={(e)=>handleChange(e)} 
-                         required/>
+                        <input pattern='[a-zA-Z]\S{1,15}'
+                            title='Solo puede contener letras sin espacios'
+                            className='d-block  m-1 border-1 form-control'
+                            type="text" value={newUser.firstName}
+                            id='firstName' name='firstName'
+                            placeholder='User Name'
+                            autoComplete='off'
+                            onChange={(e) => handleChange(e)}
+                            required />
                     </div>
 
 
 
                     <div>
-                   
-                        <label htmlFor="lastName">Last Name</label>                
-                        <input pattern="[a-zA-Z]\S{1,15}" 
-                        title='No puede contener espacios ni caracteres especiales'  
-                        className='d-block  m-1 border-1 form-control' 
-                        type="text" value={newUser.lastName} 
-                        placeholder='Last Name' 
-                        id='lastName'
-                        name='lastName'
-                        onChange={(e)=>handleChange(e)} 
-                        required/>
+
+                        <label htmlFor="lastName">Last Name</label>
+                        <input pattern="[a-zA-Z]\S{1,15}"
+                            title='No puede contener espacios ni caracteres especiales'
+                            className='d-block  m-1 border-1 form-control'
+                            type="text" value={newUser.lastName}
+                            placeholder='Last Name'
+                            id='lastName'
+                            name='lastName'
+                            onChange={(e) => handleChange(e)}
+                            required />
+                    </div>
+                </div>
+
+            </div>
+            <div className=' d-block text-center justify-content-center'>
+                <div className='col-6 justify-content-center'>
+                    <div>
+
+                        <label htmlFor="email">Email</label>
+                        <input pattern="^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+                            title="Ingrese un mail valido"
+                            className='d-block  m-1 border-1 form-control'
+                            type="email" value={newUser.email}
+                            placeholder='email@example.com'
+                            name='email'
+                            id='email'
+                            autoComplete='off'
+                            onChange={(e) => handleChange(e)}
+                            required />
+                    </div>
+
+
+                    <div>
+
+                        <label htmlFor="password">Password</label>
+                        <input pattern="^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{7,16}$"
+                            title="La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico."
+                            className='d-block  m-1 border-1 form-control'
+                            type="password"
+                            value={newUser.password}
+                            name='password'
+                            id='password'
+                            placeholder='Password'
+                            onChange={(e) => handleChange(e)}
+                            required />
                     </div>
 
                     <div>
-                       
-                        <label htmlFor="email">Email</label>                            
-                        <input pattern="^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$" 
-                        title="Ingrese un mail valido" 
-                        className='d-block  m-1 border-1 form-control' 
-                        type="email" value={newUser.email} 
-                        placeholder='email@example.com'
-                        name='email' 
-                        id='email' 
-                        autoComplete='off' 
-                        onChange={(e)=>handleChange(e)} 
-                        required/>
-                    </div>
 
-
-                    <div>
-                       
-                        <label htmlFor="password">Password</label>                     
-                        <input pattern="^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{7,16}$" 
-                        title="La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico." 
-                        className='d-block  m-1 border-1 form-control' 
-                        type="password"  
-                        value={newUser.password} 
-                        name='password' 
-                        id='password' 
-                        placeholder='Password'  
-                        onChange={(e)=>handleChange(e)} 
-                        required/>                    
-                    </div>
-
-                    <div>
-                       
                         <label htmlFor="ConfirmPassword">Confirm Password</label>
-                        <input className='d-block  m-1 border-1 form-control' 
-                        type="password"  
-                        value={newUser.ConfirmPassword} 
-                        name='ConfirmPassword' 
-                        id='ConfirmPasswordoto' 
-                        placeholder='ConfirmPassword' 
-                        onChange={(e)=>handleChange(e)}
-                         required/>
-                    
+                        <input className='d-block  m-1 border-1 form-control'
+                            type="password"
+                            value={newUser.ConfirmPassword}
+                            name='ConfirmPassword'
+                            id='ConfirmPasswordoto'
+                            placeholder='ConfirmPassword'
+                            onChange={(e) => handleChange(e)}
+                            required />
+
                     </div>
-
-
-
 
                     <div>
 
                         <label htmlFor="Photo">Photo</label>
-                        <input type="file"  
-                        accept="image/png, image/jpeg"  
-                         name='avatar' 
-                         id='avatar'  
-                         onChange={(e)=>uploadImage(e)} 
-                         required />
-                        {newUser.avatar.length ? 
-                        (<img src={newUser.avatar} width="50" height="50" alt="uploaded img" />)
-                            : null
-                          
-                        }      
-                    </div>  
-
-                   
-
-                    <div >
+                        <input type="file"
+                            accept="image/png, image/jpeg"
+                            name='avatar'
+                            id='avatar'
+                            onChange={(e) => uploadImage(e)}
+                            required />
+                        {newUser.avatar.length ?
+                            (<img src={newUser.avatar} width="50" height="50" alt="uploaded img" />)
+                            :
+                            "cargando"
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className='d-block pt-5'>
+                <div className='justify-content-center text-center'>
+                    <div>
                         <label htmlFor="acceptT" >Acepto los
-                        <button onClick={()=>showAlert()} 
-                        style={{ border:'none', backgroundColor:"white", color:"blue" }}
-                        >términos y condiciones
-                        </button> del servicio</label>
-                        <input type="checkbox"  
-                        name="acceptT" 
-                        id="acceptT" 
-                        checked={checked} 
-                        onChange={handleChangeCheckbox}/>
+                            <button onClick={() => showAlert()}
+                                style={{ border: 'none', backgroundColor: "white", color: "blue" }}
+                            >términos y condiciones
+                            </button> del servicio</label>
+                        <input type="checkbox"
+                            name="acceptT"
+                            id="acceptT"
+                            checked={checked}
+                            onChange={handleChangeCheckbox} />
                     </div>
 
-                   <button type='submit' 
-                   value='Register'  
-                   className="btn btn-primary m-2">Registrarse</button>
-                   <button type="button" 
-                   onClick={ (e) => handleRegister(e) } 
-                   className="btn btn-primary m-2">Login</button>
+                    <button type='submit'
+                        value='Register'
+                        className="btn btn-dark m-2">Registrarse</button>
+                    <button type="button"
+                        onClick={(e) => handleRegister(e)}
+                        className="btn btn-dark m-2">Volver</button>
+
+                </div>
+
+            </div>
 
 
-               </form>
-        </div> 
 
-            );
+
+        </form>
+    );
 
 
 }
