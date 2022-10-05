51382aa546adc3fbc775071cf43c7a49cc7b195e
Rodrigo Rimbau <rodrigorimbau94@gmail.com>
2022-10-05 18:54:22 -0300

    FIXED CSS IN HOME

--- client/src/componentes/Home/Home.jsx
+++ client/src/componentes/Home/Home.jsx
@@ -25,24 +25,24 @@ const Home = () => {
 
   return (
     !token ?  <Navigate to="/" replace={true} /> :
-    <div className="bg-dark">
-      <div className="row m-0 p-0 sticky-top">
+    <div className="bg-dark min-vh-100 grid">
+      <div className="g-row m-0 vh-10 p-0 sticky-top">
         <NavBar />
       </div>
-      <div className="row m-0 p-0 h-75">
+      <div className="g-row rowGridCSS d-flex m-0 p-0 h-75">
         <div className="col-2 p-0 m-0">
-          <div className="row m-0 p-0 sticky">
+          <div className="row m-0 p-0">
             <Filters />
           </div>
           <div className="row bg-dark m-0 p-0">
             <Link to="/home" className="text-decoration-none m-0 p-0">
-            <div className="col d-flex w-100 mt-5 justify-content-center ">
+            <div className="col-2 d-flex w-100 mt-5 justify-content-center ">
               
             </div>
             </Link>
           </div>
         </div>
-        <div className="col p-1 m-0 ">
+        <div className="col-10 p-1 m-0">
           {allQuestions && allQuestions.length?
           allQuestions.map((e, id) => {
             return (
@@ -59,7 +59,11 @@ const Home = () => {
          } 
         </div>
       </div>
-        <Footer/>
+      <div className="g-row vh-10 sticky-bottom m-0 p-0">
+        <div className="col m-0 p-0">
+          <Footer/>
+        </div>
+      </div>
     </div>
   );
 }
